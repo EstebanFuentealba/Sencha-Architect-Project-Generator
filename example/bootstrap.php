3c52453db168c2c988c15d03cbd65df2f1bd7bff
Esteban Fuentealba <efuentealba@json.cl>
2013-06-12 11:54:36 -0400

    [added] Example

---
+++ example/bootstrap.php
@@ -0,0 +1,9 @@
+<?PHP
+
+#	DB Config
+include(dirname(__FILE__).'/config.php');
+#	Browser Layout Project
+include(dirname(__FILE__).'/../app/app.php'); 
+echo "<b>dir build</b>: ".dirname(__FILE__).'/build<br />';
+
+?>
---
+++ example/config.php
@@ -0,0 +1,14 @@
+<?php
+//database server
+define('DB_SERVER', "localhost");
+
+//database login name
+define('DB_USER', "root");
+//database login password
+define('DB_PASS', "a");
+
+//database name
+define('DB_DATABASE', "example_db");
+
+define('PROJECT_PATH', "example/build");
+?>
